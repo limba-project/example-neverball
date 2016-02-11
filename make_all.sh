41e6cc9cb8464be47260519635db3aae52457266
Matthias Klumpp <matthias@tenstral.net>
2016-02-11 01:51:35 +0100

    Use new ExtraBundlesDir
    By using that option, we do not need to create a local Limba repository,
    or install any stuff on the host system manually. In fact, we don't even
    need to touch the host at all anymore, since all the exciting stuff now
    happens within the chroot environment.

--- make_all.sh
+++ make_all.sh
@@ -8,91 +8,83 @@ fi
 
 LB_ARGS="--chroot=$LB_CHROOT_NAME"
 
-LB_ARGS="$LB_ARGS --user=`id -u` --group=`id -g`"
+LB_ARGS="$LB_ARGS --ignore-foundations --user=`id -u` --group=`id -g`"
 
 export LIMBA_FULLNAME="Matthias Klumpp"
 export LIMBA_EMAIL="mak@debian.org"
 export LIMBA_TARGET_REPO="master"
 
+mkdir -p _bundlepool
+
 function build {
-  sudo -E limba-build run $LB_ARGS
+  sudo -E limba-build run --verbose $LB_ARGS
 }
 
-function add_to_repo {
-  for file in $1/*.ipk
-  do
-    limba-build repo-add $file
-  done
-}
-
-function install_all {
-  for file in `ls $1/*.ipk | sort -Vr`
-  do
-    sudo limba install-local $file
-  done
+function movetopool {
+  mv -f lipkg/*.ipk* ../_bundlepool
 }
 
 cd libjpeg-turbo
 find . -name "*.ipk*" -exec rm -f {} \;
 echo "Building libJPEG-Turbo"
 build
-install_all lipkg/
+movetopool
 cd ..
 
 cd libpng
 find . -name "*.ipk*" -exec rm -f {} \;
 echo "Building libpng"
 build
-install_all lipkg/
+movetopool
 cd ..
 
 cd physfs
 find . -name "*.ipk*" -exec rm -f {} \;
 echo "Building PhysFS"
 build
-install_all lipkg/
+movetopool
 cd ..
 
 cd freetype
 find . -name "*.ipk*" -exec rm -f {} \;
 echo "Building FreeType"
 build
-install_all lipkg/
+movetopool
 cd ..
 
 cd SDL2-full
 find . -name "*.ipk*" -exec rm -f {} \;
 echo "Building SDL2"
 build
-install_all lipkg/
+movetopool
 cd ..
 
 cd libogg
 find . -name "*.ipk*" -exec rm -f {} \;
 echo "Building libogg"
 build
-install_all lipkg/
+movetopool
 cd ..
 
 cd libvorbis
 find . -name "*.ipk*" -exec rm -f {} \;
 echo "Building libvorbis"
 build
-install_all lipkg/
+movetopool
 cd ..
 
-cd libXScrnSaver
-find . -name "*.ipk*" -exec rm -f {} \;
-echo "Building libXScrnSaver"
-build
-install_all lipkg/
-cd ..
+#cd libXScrnSaver
+#find . -name "*.ipk*" -exec rm -f {} \;
+#echo "Building libXScrnSaver"
+#build
+#movetopool
+#cd ..
 
 cd neverball
 find . -name "*.ipk*" -exec rm -f {} \;
 echo "Building Neverball"
 build
-install_all lipkg/
+movetopool
 cd ..
 
 echo ""
@@ -105,17 +97,10 @@ rm -rf out_lirepo
 mkdir -p out_lirepo
 cd out_lirepo
 
-#limba-build repo-init .
-add_to_repo ../libjpeg-turbo/lipkg
-add_to_repo ../libpng/lipkg
-add_to_repo ../physfs/lipkg
-add_to_repo ../freetype/lipkg
-add_to_repo ../SDL2/lipkg
-add_to_repo ../SDL2-ttf/lipkg
-add_to_repo ../libogg/lipkg
-add_to_repo ../libvorbis/lipkg
-add_to_repo ../libXScrnSaver/lipkg
-add_to_repo ../neverball/lipkg
+for fname in ../_bundlepool/*.ipk
+do
+  limba-build repo-add $fname
+done
 
 cd ..
 chmod -R 0755 out_lirepo
